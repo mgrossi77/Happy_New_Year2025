0b6e210666c0d4eda057099c997e5a734a83906a
mgrossi77 <73914461+mgrossi77@users.noreply.github.com>
2025-01-01 12:17:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
+# Happy New Year 2025 App
 import streamlit as st
+from random import randint
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Display header
+st.title("🎉 Happy New Year 2025 🎉")
+
+# Loop to display symbols
+for i in range(1, 1000):
+    space = ' ' * randint(1, 100)
+    if i % 10 == 0:
+        st.text(space + 'Happy New Year 2025 🎉')
+    elif i % 9 == 0:
+        st.text(space + "🪅")
+    elif i % 5 == 0:
+        st.text(space + "🎈")
+    elif i % 8 == 0:
+        st.text(space + "🎈")
+    elif i % 7 == 0:
+        st.text(space + "🍁")
+    elif i % 6 == 0:
+        st.text(space + "❤️")
+    else:
+        st.text(space + "🔸")
